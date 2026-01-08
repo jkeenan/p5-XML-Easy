5b877876514f7c2e104dcbb5c4d5230ea3823a69
James E Keenan <jkeenan@cpan.org>
2026-01-08 14:19:52 +0000

    CPAN version 0.005

---
+++ t/setup_pp.pl
@@ -0,0 +1,10 @@
+require XSLoader;
+
+my $orig_load = \&XSLoader::load;
+no warnings "redefine";
+*XSLoader::load = sub {
+	die "XS loading disabled for XML::Easy" if $_[0] eq "XML::Easy";
+	goto &$orig_load;
+};
+
+1;
